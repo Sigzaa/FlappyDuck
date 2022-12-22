21f6d6947f83fa9fc6230b45db7ba071cc18ebc4
ggaast <gaast.dir@gmail.com>
2022-12-22 15:12:22 +0100

    #2

--- Wall/Scripts/Obstacle.gd
+++ Wall/Scripts/Obstacle.gd
@@ -2,9 +2,10 @@ extends Node2D
 
 signal score
 
-const SPEED = 200
 
-func _physics_process(delta):
-	position.x += -SPEED * delta
-	if global_position.x <= -200:
-		queue_free()
+
+	
+
+		
+func stop_moving():
+	pass
--- Wall/Scripts/ObstacleSpawner.gd
+++ Wall/Scripts/ObstacleSpawner.gd
@@ -3,20 +3,43 @@ extends Node2D
 onready var timer = $Timer
 var Obstacle = preload("res://Scenes/Obstacle.tscn")
 
+# Another node in the scene can be exported as a NodePath.
+export(NodePath) var duck_path
+# Do take note that the node itself isn't being exported -
+# there is one more step to call the true node:
+onready var duck = get_node(duck_path)
+
+export var speed = 200
+
+var is_moving = false
+
 func _ready():
 	randomize()
 
 func _on_Timer_timeout():
-	spawn_obstacle()
+	if is_moving:
+		spawn_obstacle()
 
 func spawn_obstacle():
 	var obstacle = Obstacle.instance()
 	add_child(obstacle)
 	obstacle.position.x = 1100
 	obstacle.position.y = randi()%200 - randi()%200
+
+
+func despawn_all():
+	for child in get_tree().get_nodes_in_group("tube"):
+		child.queue_free()
+
+func _physics_process(delta):
 	
-func start():
-	timer.start()
+	for child in get_tree().get_nodes_in_group("coll"):
+		
+		if is_moving:
+			
+			child.position.x -= speed * delta
+		
+		if child.global_position.x <= -200:
+			
+			child.queue_free()
 	
-func stop():
-	timer.stop()
